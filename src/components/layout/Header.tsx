b65dfef22bc92e0413bd922b022840a3aa4aeb0d
Jimmy <jimmy.b.mau@gmail.com>
2023-12-11 13:04:08 -0500

    feat: add header wireframe

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,27 @@
+import Link from 'next/link'
+import { Input } from "@/components/ui/input"
+import HamburgerMenu from "@/components/HamburgerMenu"
+
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className="flex flex-row bg-red-300 items-center justify-between px-3 py-2">
+      <div className="flex flex-row space-x-3 items-center">
+        <Link href="/">
+          LOGO
+        </Link>
+        <Input placeholder="Search markets" />
+      </div>
+      <div className="space-x-3">
+        <Link href="/" className="hover:cursor-pointer underline hover:no-underline">Fair Launch</Link>
+        <Link href="/" className="hover:cursor-pointer underline hover:no-underline">Markets</Link>
+        <Link href="/" className="hover:cursor-pointer underline hover:no-underline">Insight</Link>
+        <Link href="/" className="hover:cursor-pointer underline hover:no-underline">FAQ</Link>
+        <Link href="/" className="hover:cursor-pointer underline hover:no-underline">Leaderboard</Link>
+      </div>
+      <div className="flex flex-row items-center justify-center space-x-3">
+        <Link href="/" className="hover:cursor-pointer underline hover:no-underline">Sign Up</Link>
+        <HamburgerMenu />
+      </div>
+    </header>
+  );
 }

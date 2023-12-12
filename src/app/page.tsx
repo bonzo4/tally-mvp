7abd432cd3708a809627bc2dec42121cd612445a
Jimmy <jimmy.b.mau@gmail.com>
2023-12-12 12:29:16 -0500

    feat: WIP add landing page 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 
 function Tickers() {
   return (
-    <div className="w-full min-h-[5vh] bg-purple-50">
+    <div className="w-full min-h-[8vh] bg-purple-50">
       <p>Tickers</p>
     </div>
   )
@@ -63,8 +63,51 @@ function PredictionMarkets() {
 
 function LiveNewsFeed() {
   return(
-    <div>
-      <h2 className="bg-purple-100 text-4xl font-bold">Live News Feed</h2>
+    <div className="flex flex-col h-[80vh] md:h-full space-y-5">
+      <div>
+        <h2 className="text-4xl font-bold">Live News Feed</h2>
+      </div>
+      <div className="bg-purple-100 flex-1">
+      </div>
+    </div>
+  )
+}
+
+function Insight() {
+  return (
+    <div className="bg-pink-200 w-full h-[20vh]">
+    </div>
+  )
+}
+
+function Insights() {
+  return (
+    <div className="flex flex-col h-full space-y-5">
+      <div>
+        <h3 className="text-2xl">The Latest</h3>
+        <h2 className="text-4xl font-bold">Insights</h2>
+      </div>
+      <div className="space-y-5">
+        <Insight />
+        <Insight />
+        <Insight />
+        <Insight />
+      </div>
+      <Link href="/">
+        <Button>View All</Button>
+      </Link>
+    </div>
+  )
+}
+
+function Guide() {
+  return (
+    <div className="flex flex-col h-[80vh] md:h-full space-y-5">
+      <div>
+        <h2 className="text-4xl font-bold">Guide</h2>
+      </div>
+      <div className="bg-blue-100 flex-1">
+      </div>
     </div>
   )
 }
@@ -74,13 +117,23 @@ export default function LandingPage() {
     <div className="w-full">
       <Tickers />
       <Banner />
-      <div className="w-full flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5 px-10 py-10">
-        <div className="md:w-[60vw] space-y-5">
-          <FairLaunch />
-          <PredictionMarkets />
+      <div className="w-full flex flex-col space-y-5 px-10 py-10">
+        <div className="w-full flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5">
+          <div className="md:w-[60vw] space-y-5">
+            <FairLaunch />
+            <PredictionMarkets />
+          </div>
+          <div className="md:w-[40vw]">
+            <LiveNewsFeed />
+          </div>
         </div>
-        <div className="md:w-[40vw]">
-          <LiveNewsFeed />
+        <div className="w-full flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5">
+          <div className="md:w-[60vw] space-y-5">
+            <Insights />
+          </div>
+          <div className="md:w-[40vw]">
+            <Guide />
+          </div>
         </div>
       </div>
     </div>

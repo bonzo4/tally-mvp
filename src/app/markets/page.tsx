69b70334f4687d35a9503cc2151b2eb6e63e3aae
Jimmy <jimmy.b.mau@gmail.com>
2023-12-13 12:34:39 -0500

    refactor: MarketTeaser to be used on landing page

--- src/app/markets/page.tsx
+++ src/app/markets/page.tsx
@@ -1,8 +1,7 @@
-import Link from "next/link";
-import Image from "next/image";
 
 import Banner from "@/components/landing/Banner";
 import Tickers from "@/components/landing/Tickers";
+import { MarketTeaserProps, MarketTeaser } from "@/components/MarketTeaser";
 
 const TEST_MARKET_TEASER_DATA: MarketTeaserProps[] = [
   {
@@ -41,56 +40,6 @@ const TEST_MARKET_TEASER_DATA: MarketTeaserProps[] = [
     noPrice: 72,
   },
 ]
-
-interface MarketTeaserProps {
-  title: string;
-  category: string;
-  image: string;
-  yesPrice: number;
-  noPrice: number;
-}
-
-function MarketTeaser({ title, category, image, yesPrice, noPrice }: MarketTeaserProps) {
-  return (
-    <div>
-      <Link href="/">
-        <div className="flex flex-col h-full border border-gray-100 rounded shadow space-y-2 p-3">
-          <div className="flex space-x-3 w-full h-2/3">
-            <div>
-              <div className="relative w-[75px] h-[75px]">
-                <Image 
-                  src={image}
-                  fill={true}
-                  alt="test image"
-                  className="object-cover"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col">
-              <h2 className="text-gray-600 text-xs">{category}</h2>
-              <div className="justify-self-center overflow-clip">
-                <h1 className="font-bold">{title}</h1>
-              </div>
-            </div>
-          </div>
-          <div className="flex w-full h-[50px] relative space-x-1">
-            <div style={{ width: `${yesPrice}%` }} className="bg-green-100 h-full rounded-l"></div>
-            <div style={{ width: `${noPrice}%` }} className="bg-red-100 h-full rounded-r"></div>
-
-            <div className="absolute top-0 left-0 w-full h-full flex items-center px-2">
-              <span className="text-green-600 font-semibold whitespace-nowrap absolute left-2">
-                Yes: {yesPrice}¢
-              </span>
-              <span className="text-red-600 font-semibold whitespace-nowrap absolute right-2">
-                No: {noPrice}¢
-              </span>
-            </div>
-          </div>
-        </div>
-      </Link>
-    </div>
-  )
-}
 
 function MarketsGallery() {
   return (

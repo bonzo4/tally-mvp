46f459e0e29ba34185de20ca9a7983bed87a8c53
Jimmy <jimmy.b.mau@gmail.com>
2023-12-12 19:58:26 -0500

    feat: add markets text and image placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,35 @@ function FairLaunch() {
 
 function Markets() {
   return (
-    <div className="bg-green-100 h-[30vw] md:h-[10vw]">
+    <div>
+      <Link href="/">
+        <div className="bg-green-100 flex flex-col h-[40vw] md:h-[15vw] space-y-2 p-2">
+          <div className="flex space-x-2 w-full h-2/3">
+            <div className="relative flex w-1/3">
+              <Image 
+                src="https://raw.githubusercontent.com/davidjerleke/embla-carousel/master/packages/embla-carousel-docs/src/assets/images/slide-2.jpg" 
+                fill={true}
+                alt="test image"
+                className="object-cover"
+              />
+            </div>
+            <div className="flex flex-col w-2/3 justify-center">
+              <h2 className="text-gray-600">Politics</h2>
+              <div className="">
+                <h1 className="font-bold">Will Trump win the Republican Nominee?</h1>
+              </div>
+            </div>
+          </div>
+          <div className="flex w-full h-1/3">
+            <div className="bg-green-300 w-1/2 h-full">
+              Yes: 50%
+            </div>
+            <div className="bg-red-300 w-1/2 h-full">
+              No: 50%
+            </div>
+          </div>
+        </div>
+      </Link>
     </div>
   )
 }
@@ -44,7 +72,7 @@ function PredictionMarkets() {
       <div>
         <h2 className="text-4xl font-bold">Prediction Markets</h2>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
         <Markets />
         <Markets />
         <Markets />

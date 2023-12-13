1185ba944405859b84a3a78d2a355717647dbcbd
Jimmy <jimmy.b.mau@gmail.com>
2023-12-12 20:04:20 -0500

    feat: add lorem ipsum to guide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -158,7 +158,23 @@ function Guide() {
       <div>
         <h2 className="text-4xl font-bold">Guide</h2>
       </div>
-      <div className="bg-blue-100 flex-1">
+      <div className="bg-blue-100 flex-1 p-3">
+        <h1 className="text-xl font-bold">Instructions:</h1>
+        <p>
+          <ol className="list-decimal list-inside mb-3">
+            <li>Lorem ipsum </li>
+            <li>Sed et eros id massa dictum semper</li>
+            <li>Vestibulum quis tortor a sem lacinia finibus quis et est. Nulla suscipit diam ac interdum aliquam. Donec tristique, lectus quis molestie vestibulum, dui nibh venenatis dolor, non interdum erat turpis ut lectus.</li>
+          </ol>
+        </p>
+        <h1 className="text-xl font-bold">More Instructions:</h1>
+        <p>
+          <ol className="list-decimal list-inside">
+            <li>Lorem ipsum </li>
+            <li>Sed et eros id massa dictum semper</li>
+            <li>Vestibulum quis tortor a sem lacinia finibus quis et est. Nulla suscipit diam ac interdum aliquam. Donec tristique, lectus quis molestie vestibulum, dui nibh venenatis dolor, non interdum erat turpis ut lectus.</li>
+          </ol>
+        </p>
       </div>
     </div>
   )

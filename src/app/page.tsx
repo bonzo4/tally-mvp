ee9b1a5c66f557190d07846eb2d5bef718b376d6
Jimmy <jimmy.b.mau@gmail.com>
2023-12-12 22:01:25 -0500

    fix: hydration issue with <ol> wrapped in <p>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -160,21 +160,17 @@ function Guide() {
       </div>
       <div className="bg-blue-100 flex-1 p-3">
         <h1 className="text-xl font-bold">Instructions:</h1>
-        <p>
-          <ol className="list-decimal list-inside mb-3">
-            <li>Lorem ipsum </li>
-            <li>Sed et eros id massa dictum semper</li>
-            <li>Vestibulum quis tortor a sem lacinia finibus quis et est. Nulla suscipit diam ac interdum aliquam. Donec tristique, lectus quis molestie vestibulum, dui nibh venenatis dolor, non interdum erat turpis ut lectus.</li>
-          </ol>
-        </p>
+        <ol className="list-decimal list-inside mb-3">
+          <li>Lorem ipsum </li>
+          <li>Sed et eros id massa dictum semper</li>
+          <li>Vestibulum quis tortor a sem lacinia finibus quis et est. Nulla suscipit diam ac interdum aliquam. Donec tristique, lectus quis molestie vestibulum, dui nibh venenatis dolor, non interdum erat turpis ut lectus.</li>
+        </ol>
         <h1 className="text-xl font-bold">More Instructions:</h1>
-        <p>
-          <ol className="list-decimal list-inside">
-            <li>Lorem ipsum </li>
-            <li>Sed et eros id massa dictum semper</li>
-            <li>Vestibulum quis tortor a sem lacinia finibus quis et est. Nulla suscipit diam ac interdum aliquam. Donec tristique, lectus quis molestie vestibulum, dui nibh venenatis dolor, non interdum erat turpis ut lectus.</li>
-          </ol>
-        </p>
+        <ol className="list-decimal list-inside">
+          <li>Lorem ipsum </li>
+          <li>Sed et eros id massa dictum semper</li>
+          <li>Vestibulum quis tortor a sem lacinia finibus quis et est. Nulla suscipit diam ac interdum aliquam. Donec tristique, lectus quis molestie vestibulum, dui nibh venenatis dolor, non interdum erat turpis ut lectus.</li>
+        </ol>
       </div>
     </div>
   )

8be330ba188120e1d3b34f7cabfa76b8519c22bd
Jimmy <jimmy.b.mau@gmail.com>
2023-12-14 14:22:42 -0500

    feat: add leaderboard page and components

---
+++ src/app/leaderboard/components/Leaderboard.tsx
@@ -0,0 +1,29 @@
+import LeaderboardItem from "./LeaderboardItem"
+
+export default function Leaderboard({ title }: { title: string }) {
+  return (
+    <div className="flex flex-col rounded md:border md:border-gray-200 md:shadow-md space-y-2 lg:space-y-5 p-1 md:p-5">
+      <div>
+        <h2 className="text-xl lg:text-2xl font-bold">{ title }</h2>
+      </div>
+      <div className="flex flex-col space-y-3">
+        <LeaderboardItem rank={1} name="satoshi nakamoto" value={1000000000}/>
+        <LeaderboardItem rank={2} name="i have a really really long name and i don't really know why but it's an edge case" value={218392}/>
+        <LeaderboardItem rank={3} name="Vitalik Buterin" value={199801}/>
+        <LeaderboardItem rank={4} name="Richard Feynman" value={38579}/>
+        <LeaderboardItem rank={5} name="Richard Feynman" value={38579}/>
+        <LeaderboardItem rank={6} name="Richard Feynman" value={38579}/>
+        <LeaderboardItem rank={7} name="Richard Feynman" value={38579}/>
+        <LeaderboardItem rank={8} name="Richard Feynman" value={38579}/>
+        <LeaderboardItem rank={9} name="Richard Feynman" value={38579}/>
+        <LeaderboardItem rank={10} name="Marcus Aurelius" value={164}/>
+        <LeaderboardItem rank={11} name="Marcus Aurelius" value={164}/>
+        <LeaderboardItem rank={12} name="Marcus Aurelius" value={164}/>
+        <LeaderboardItem rank={13} name="Marcus Aurelius" value={164}/>
+        <LeaderboardItem rank={14} name="Marcus Aurelius" value={164}/>
+        <LeaderboardItem rank={15} name="Marcus Aurelius" value={164}/>
+      </div>
+    </div>
+  )
+}
+
---
+++ src/app/leaderboard/components/LeaderboardItem.tsx
@@ -0,0 +1,39 @@
+import MedalRankDisplay from "./MedalRankDisplay"
+
+export default function LeaderboardItem({ name, rank, value }: { name: string, rank: number, value: number }) {
+  const valueFormatted = new Intl.NumberFormat(
+    'en-US', 
+    { 
+      style: 'currency', 
+      currency: 'USD', 
+      maximumFractionDigits: 0, 
+      minimumFractionDigits: 0 
+    }
+  ).format(value)
+
+  let backgroundColor: string;
+  switch (rank) {
+    case 1:
+      backgroundColor = "bg-yellow-100"
+      break;
+    case 2:
+      backgroundColor = "bg-gray-100"
+      break;
+    case 3:
+      backgroundColor = "bg-orange-50"
+      break;
+    default:
+      backgroundColor = ""
+  }
+
+  return (
+    <div className={`w-full flex flex-row items-center px-2 space-x-2 rounded ${backgroundColor}`}>
+      <MedalRankDisplay rank={rank} />
+      <div className="grid grid-cols-1 md:grid-cols-[auto,1fr] md:gap-2 w-full items-center">
+        <span className="text-left text-sm lg:text-base font-bold truncate">{name}</span>
+        <span className="text-left md:text-right text-sm text-gray-500">{valueFormatted}</span>
+      </div>
+    </div>
+  )
+}
+

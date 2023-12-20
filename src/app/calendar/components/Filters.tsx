c386a5566e2c3b8ebec8e3659303663cae097e29
Jimmy <jimmy.b.mau@gmail.com>
2023-12-20 15:50:29 -0800

    feat: add calendar page
    Filter buttons are functional.

---
+++ src/app/calendar/components/Filters.tsx
@@ -0,0 +1,27 @@
+import { Button } from '@/components/ui/button'
+
+interface FilterProps {
+  filterMonth: string,
+  setFilterMonth: (month: string) => void
+}
+
+export default function Filters({ filterMonth, setFilterMonth }: FilterProps ) {
+  return (
+    <div className="space-x-2">
+      <Button variant={filterMonth === "All" ? "default" : "secondary"} onClick={() => setFilterMonth("All")}>All</Button>
+      <Button variant={filterMonth === "Jan" ? "default" : "secondary"} onClick={() => setFilterMonth("Jan")}>Jan</Button>
+      <Button variant={filterMonth === "Feb" ? "default" : "secondary"} onClick={() => setFilterMonth("Feb")}>Feb</Button>
+      <Button variant={filterMonth === "Mar" ? "default" : "secondary"} onClick={() => setFilterMonth("Mar")}>Mar</Button>
+      <Button variant={filterMonth === "Apr" ? "default" : "secondary"} onClick={() => setFilterMonth("Apr")}>Apr</Button>
+      <Button variant={filterMonth === "May" ? "default" : "secondary"} onClick={() => setFilterMonth("May")}>May</Button>
+      <Button variant={filterMonth === "Jun" ? "default" : "secondary"} onClick={() => setFilterMonth("Jun")}>Jun</Button>
+      <Button variant={filterMonth === "Jul" ? "default" : "secondary"} onClick={() => setFilterMonth("Jul")}>Jul</Button>
+      <Button variant={filterMonth === "Aug" ? "default" : "secondary"} onClick={() => setFilterMonth("Aug")}>Aug</Button>
+      <Button variant={filterMonth === "Sep" ? "default" : "secondary"} onClick={() => setFilterMonth("Sep")}>Sep</Button>
+      <Button variant={filterMonth === "Oct" ? "default" : "secondary"} onClick={() => setFilterMonth("Oct")}>Oct</Button>
+      <Button variant={filterMonth === "Nov" ? "default" : "secondary"} onClick={() => setFilterMonth("Nov")}>Nov</Button>
+      <Button variant={filterMonth === "Dec" ? "default" : "secondary"} onClick={() => setFilterMonth("Dec")}>Dec</Button>
+    </div>
+  )
+}
+
---
+++ src/app/calendar/page.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { useEffect, useState } from 'react'
+
+import Overview from './components/Overview'
+import Filters from './components/Filters'
+import Month from './components/Month'
+
+const TEST_EVENT_DATA = [
+  {
+    month: "JAN",
+    day: 10,
+    name: "Republican presidential debate hosted by CNN.",
+    description: "A Republican presidential primary debate will take place at Drake University in Des Moine, Iowa.",
+    src: "/",
+    reminder: "/",
+    is_highlighted: true
+  },
+  {
+    month: "JAN",
+    day: 15,
+    name: "Iowa Republican caucuses.",
+    description: "",
+    reminder: "/",
+    is_highlighted: false
+  },
+  {
+    month: "JAN",
+    day: 21,
+    name: "Republican presidential debate hosted by CNN.",
+    description: "A Republican presidential primary debate will take place at Drake University in Des Moine, Iowa.",
+    src: "/",
+    reminder: "/",
+    is_highlighted: true
+  },
+  {
+    month: "JAN",
+    day: 23,
+    name: "New Hampshire Republican presidential primary election.",
+    description: "Democratic primary is non-binding.",
+    reminder: "/",
+    is_highlighted: false
+  },
+  {
+    month: "JAN",
+    day: 31,
+    name: "FEC year-end report due.",
+    description: "For presidential and congressional candidate committees.",
+    reminder: "/",
+    is_highlighted: false
+  },
+  {
+    month: "FEB",
+    day: 3,
+    name: "South Carolina Democratic presidential primary election",
+    description: "",
+    reminder: "/",
+    is_highlighted: false
+  },
+  {
+    month: "FEB",
+    day: 6,
+    name: "Nevada Democratic presidential primary election",
+    description: "Republican primary is non-binding.",
+    reminder: "/",
+    is_highlighted: false
+  },
+]
+
+export default function Calendar() {
+  const [ filterMonth, setFilterMonth ] = useState("All")
+
+  const TEST_JANUARY_DATA = TEST_EVENT_DATA.filter((event) => event.month === "JAN")
+  const TEST_FEBRUARY_DATA = TEST_EVENT_DATA.filter((event) => event.month === "FEB")
+  const TEST_MARCH_DATA = TEST_EVENT_DATA.filter((event) => event.month === "MAR")
+
+  return (
+    <div className="w-full flex flex-col px-2 py-5 lg:px-10 space-y-5">
+      <Overview />
+      <Filters filterMonth={filterMonth} setFilterMonth={setFilterMonth}  />
+      { filterMonth === "All" || filterMonth === "Jan" ? (<Month name="January 2024" events={TEST_JANUARY_DATA} />) : null }
+      { filterMonth === "All" || filterMonth === "Feb" ? (<Month name="February 2024" events={TEST_FEBRUARY_DATA} />) : null }
+      { filterMonth === "All" || filterMonth === "Mar" ? (<Month name="March 2024" events={TEST_MARCH_DATA} />) : null }
+    </div>
+  )
+}

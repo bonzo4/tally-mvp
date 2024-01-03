86d55832d2e5dec93459f3b3d6f3ef951c227a38
Jimmy <jimmy.b.mau@gmail.com>
2024-01-03 15:34:10 -0800

    style: how to section in landing page

--- src/app/components/Guide.tsx
+++ src/app/components/Guide.tsx
@@ -1,23 +1,45 @@
+import { MdAttachMoney } from "react-icons/md";
+
+function Step1() {
+  return (
+    <div className="flex flex-col items-center space-y-3">
+      <div className="flex">
+        <div className="border-2 border-tally-primary rounded-full p-4">
+          <MdAttachMoney className="text-tally-primary text-[4em]"/>
+        </div>
+      </div>
+      <div>
+        <h3 className="text-2xl text-white font-bold">Step 1</h3>
+      </div>
+      <div className="lg:max-w-[250px]">
+        <p className="text-center text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+      </div>
+    </div>
+  )
+}
+
+function Illustrations() {
+  return (
+    <div className="w-4/5 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5 mt-16">
+      <Step1 />
+      <Step1 />
+      <Step1 />
+      <Step1 />
+    </div>
+  )
+}
 export default function Guide() {
   return (
-    <div className="flex flex-col h-[80vh] md:h-full space-y-5">
-      <div>
-        <h2 className="text-4xl font-bold">Guide</h2>
+    <div className="flex flex-col justify-content items-center px-4 lg:px-16 py-16">
+      <div className="mb-6">
+        <h2 className="text-2xl lg:text-4xl text-white font-bold text-center">How It Works</h2>
       </div>
-      <div className="bg-blue-100 flex-1 p-3">
-        <h1 className="text-xl font-bold">Instructions:</h1>
-        <ol className="list-decimal list-inside mb-3">
-          <li>Lorem ipsum </li>
-          <li>Sed et eros id massa dictum semper</li>
-          <li>Vestibulum quis tortor a sem lacinia finibus quis et est. Nulla suscipit diam ac interdum aliquam. Donec tristique, lectus quis molestie vestibulum, dui nibh venenatis dolor, non interdum erat turpis ut lectus.</li>
-        </ol>
-        <h1 className="text-xl font-bold">More Instructions:</h1>
-        <ol className="list-decimal list-inside">
-          <li>Lorem ipsum </li>
-          <li>Sed et eros id massa dictum semper</li>
-          <li>Vestibulum quis tortor a sem lacinia finibus quis et est. Nulla suscipit diam ac interdum aliquam. Donec tristique, lectus quis molestie vestibulum, dui nibh venenatis dolor, non interdum erat turpis ut lectus.</li>
-        </ol>
+      <div className="max-w-[700px]">
+        <p className="text-white text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.
+        </p>
       </div>
+      <Illustrations />
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,13 +25,7 @@ export default async function LandingPage() {
         <PredictionMarkets subMarkets={subMarkets} />
         <Insights />
         <LiveNewsFeed />
-        <div className="w-full flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5">
-          <div className="md:w-[60vw] space-y-5">
-          </div>
-          <div className="md:w-[40vw]">
-            <Guide />
-          </div>
-        </div>
+        <Guide />
       </div>
     </div>
   );

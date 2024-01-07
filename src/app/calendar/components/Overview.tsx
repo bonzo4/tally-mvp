7cfe88be0904aef30cedf94a91656473fef47464
Jimmy <jimmy.b.mau@gmail.com>
2024-01-07 15:25:32 -0800

    style: calendar title and overview

--- src/app/calendar/components/Overview.tsx
+++ src/app/calendar/components/Overview.tsx
@@ -3,17 +3,17 @@ export default function Overview() {
     <div className="flex max-w-[800px] flex-col space-y-5">
       <div className="flex flex-col lg:flex-row lg:items-end lg:space-x-3">
         <div>
-          <h1 className="text-2xl font-bold lg:text-4xl">
+          <h1 className="text-2xl font-bold text-white lg:text-4xl">
             2024 Election Calendar
           </h1>
         </div>
         <div>
-          <div className="inline-block border-b border-gray-600 text-xs text-gray-600 lg:text-base">
+          <div className="inline-block text-xs text-gray-400 lg:text-base">
             Last Updated: Dec 19, 2023
           </div>
         </div>
       </div>
-      <div>
+      <div className="text-base text-gray-400 lg:text-lg">
         Explore key dates, events and deadlines for the 2024 and other US
         elections. Some of these dates are subject to change. Check back
         frequently for the latest updates.

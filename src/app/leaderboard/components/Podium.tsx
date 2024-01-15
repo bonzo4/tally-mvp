a0213173e426118318fdf90cd888e2cba99acb01
Jimmy <jimmy.b.mau@gmail.com>
2024-01-15 14:32:27 -0500

    style: truncate username if too long

--- src/app/leaderboard/components/Podium.tsx
+++ src/app/leaderboard/components/Podium.tsx
@@ -7,8 +7,8 @@ function Place({ place, user }: { place: string; user: LeaderboardType }) {
   const { username, image, volume } = user;
   return (
     <div className="flex w-full justify-between rounded-2xl border-2 border-tally-primary px-6 py-4">
-      <div className="flex">
-        <div className="relative h-[82px] w-[82px]">
+      <div className="flex truncate">
+        <div className="relative h-[82px] w-[82px] flex-shrink-0">
           <Image
             src={image}
             alt=""
@@ -16,14 +16,14 @@ function Place({ place, user }: { place: string; user: LeaderboardType }) {
             className="rounded-xl object-cover"
           />
         </div>
-        <div className="relative flex items-center justify-center">
-          <p className="ml-4 text-2xl text-white">{username}</p>
+        <div className="relative flex items-center justify-center truncate">
+          <p className="ml-4 truncate text-2xl text-white">{username}</p>
           <div className="absolute left-4 text-7xl font-bold text-tally-primary/15">
             {place}
           </div>
         </div>
       </div>
-      <div className="flex flex-col items-end justify-center">
+      <div className="flex flex-shrink-0 flex-col items-end justify-center">
         <div className="text-sm font-bold text-white">
           <span>Total Volume</span>
         </div>
@@ -41,7 +41,7 @@ export default function Podium({
   leaderboard: LeaderboardType[];
 }) {
   return (
-    <div className="flex grid w-full grid-cols-3 gap-4">
+    <div className="grid w-full grid-cols-3 gap-4">
       <Place place="1st" user={leaderboard[0]} />
       <Place place="2nd" user={leaderboard[1]} />
       <Place place="3rd" user={leaderboard[2]} />

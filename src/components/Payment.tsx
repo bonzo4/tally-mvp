f1ca780d2fb4ca101c9a2286fc1eddbbccbc4a23
bonzo4 <alonzo.sabino814@gmail.com>
2024-01-17 09:38:27 -0600

    fix: payment refactor

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -10,35 +10,41 @@ type PaymentProps = {
 export default function Payment() {
   const [amount, setAmount] = useState("5");
 
-  const helioConfig = {
-    paylinkId: process.env.NEXT_PUBLIC_HELIO_PAYLINK_ID!,
-    themeMode: { colors: { primaryButtonBackground: "46FF9B" } },
-    amount: amount,
-    // display: "button",
-    onSuccess: (event: {
-      data: unknown;
-      transaction: string;
-      paymentPK?: string;
-      swapTransactionSignature?: string;
-      blockchainSymbol?: string;
-    }) => console.log(event),
-    onError: (event: { transaction?: string; errorMessage?: string }) =>
-      console.log(event),
-    onPending: (event: { data?: unknown; transaction: string }) =>
-      console.log(event),
-    onCancel: () => console.log("Cancelled payment"),
-    onStartPayment: () => console.log("Starting payment"),
-  };
-
   const onAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (parseInt(event.target.value) < 0) setAmount("0.01");
-    else setAmount(event.target.value);
+    setAmount(event.target.value);
   };
 
   return (
     <>
       <Input value={amount} onChange={onAmountChange} type="number" />
-      <HelioCheckout config={helioConfig} />
+      <HelioCheckout
+        config={{
+          paylinkId: process.env.NEXT_PUBLIC_HELIO_PAYLINK_ID!,
+          network: "test",
+          theme: {
+            themeMode: "dark",
+            colors: {
+              primaryButtonBackground: "46FF9B",
+              primaryButtonText: "FFFFF",
+            },
+          },
+          amount: amount,
+          // display: "button",
+          onSuccess: (event: {
+            data: unknown;
+            transaction: string;
+            paymentPK?: string;
+            swapTransactionSignature?: string;
+            blockchainSymbol?: string;
+          }) => console.log(event),
+          onError: (event: { transaction?: string; errorMessage?: string }) =>
+            console.log(event),
+          onPending: (event: { data?: unknown; transaction: string }) =>
+            console.log(event),
+          onCancel: () => console.log("Cancelled payment"),
+          onStartPayment: () => console.log("Starting payment"),
+        }}
+      />
     </>
   );
 }

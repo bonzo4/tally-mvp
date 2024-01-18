11cab709038304556385a147bdedaae4755ee938
Jimmy <jimmy.b.mau@gmail.com>
2024-01-18 16:49:34 -0500

    feat: add grid version of fair launch

--- src/app/fair-launch/[slug]/components/OrderCards.tsx
+++ src/app/fair-launch/[slug]/components/OrderCards.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import { formatDollarsWithoutCents } from "@/lib/formats";
 import { cn } from "@/lib/utils";
@@ -10,37 +11,49 @@ import { cn } from "@/lib/utils";
 import { FilterButton } from "@/components/FilterButton";
 import { ChoiceMarket } from "@/lib/supabase/markets/subMarkets";
 
-function OrderButton({ choice }: { choice: ChoiceMarket }) {
-  const borderCssMap = {
-    main: "border-tally-primary hover:border-tally-primary/90",
-    red: "border-tally-red hover:border-tally-red/90",
-    orange: "border-tally-orange hover:border-tally-orange/90",
-    yellow: "border-tally-yellow hover:border-tally-yellow/90",
-    green: "border-tally-green hover:border-tally-green/90",
-    blue: "border-tally-blue hover:border-tally-blue/90",
-    purple: "border-tally-purple hover:border-tally-purple/90",
-    indigo: "border-tally-indigo hover:border-tally-indigo/90",
-    gray: "border-tally-gray hover:border-tally-gray/90",
-  };
+const borderCssMap = {
+  main: "border-tally-primary hover:border-tally-primary/90",
+  red: "border-tally-red hover:border-tally-red/90",
+  orange: "border-tally-orange hover:border-tally-orange/90",
+  yellow: "border-tally-yellow hover:border-tally-yellow/90",
+  green: "border-tally-green hover:border-tally-green/90",
+  blue: "border-tally-blue hover:border-tally-blue/90",
+  purple: "border-tally-purple hover:border-tally-purple/90",
+  indigo: "border-tally-indigo hover:border-tally-indigo/90",
+  gray: "border-tally-gray hover:border-tally-gray/90",
+};
 
+const buttonCssMap = {
+  primary: "bg-tally-primary hover:bg-tally-primary/90",
+  red: "bg-tally-red hover:bg-tally-red/90",
+  orange: "bg-tally-orange hover:bg-tally-orange/90",
+  yellow: "bg-tally-yellow hover:bg-tally-yellow/90",
+  green: "bg-tally-green hover:bg-tally-green/90",
+  blue: "bg-tally-blue hover:bg-tally-blue/90",
+  purple: "bg-tally-purple hover:bg-tally-purple/90",
+  indigo: "bg-tally-indigo hover:bg-tally-indigo/90",
+  gray: "bg-tally-gray hover:bg-tally-gray/90",
+};
+
+const textCssMap = {
+  primary: "text-tally-primary",
+  red: "text-tally-red",
+  orange: "text-tally-orange",
+  yellow: "text-tally-yellow",
+  green: "text-tally-green",
+  blue: "text-tally-blue",
+  purple: "text-tally-purple",
+  indigo: "text-tally-indigo",
+  gray: "text-tally-gray",
+};
+
+function OrderCardGrid({ choice }: { choice: ChoiceMarket }) {
   let borderCss = "border-tally-primary hover:border-tally-primary/90";
   if (choice.title === "No") {
     borderCss = "border-tally-red hover:border-tally-red/90";
   } else if (borderCssMap[choice.color as keyof typeof borderCssMap]) {
     borderCss = borderCssMap[choice.color as keyof typeof borderCssMap];
   }
-
-  const buttonCssMap = {
-    primary: "bg-tally-primary hover:bg-tally-primary/90",
-    red: "bg-tally-red hover:bg-tally-red/90",
-    orange: "bg-tally-orange hover:bg-tally-orange/90",
-    yellow: "bg-tally-yellow hover:bg-tally-yellow/90",
-    green: "bg-tally-green hover:bg-tally-green/90",
-    blue: "bg-tally-blue hover:bg-tally-blue/90",
-    purple: "bg-tally-purple hover:bg-tally-purple/90",
-    indigo: "bg-tally-indigo hover:bg-tally-indigo/90",
-    gray: "bg-tally-gray hover:bg-tally-gray/90",
-  };
 
   let buttonCss = "bg-tally-primary hover:bg-tally-primary/90";
   if (choice.title === "No") {
@@ -49,17 +62,77 @@ function OrderButton({ choice }: { choice: ChoiceMarket }) {
     buttonCss = buttonCssMap[choice.color as keyof typeof buttonCssMap];
   }
 
-  const textCssMap = {
-    primary: "text-tally-primary",
-    red: "text-tally-red",
-    orange: "text-tally-orange",
-    yellow: "text-tally-yellow",
-    green: "text-tally-green",
-    blue: "text-tally-blue",
-    purple: "text-tally-purple",
-    indigo: "text-tally-indigo",
-    gray: "text-tally-gray",
-  };
+  let textCss = "text-tally-primary";
+  if (choice.title === "No") {
+    textCss = "text-tally-red";
+  } else if (textCssMap[choice.color as keyof typeof textCssMap]) {
+    textCss = textCssMap[choice.color as keyof typeof textCssMap];
+  }
+
+  return (
+    <div
+      className={cn(
+        "flex justify-between space-x-8 rounded-2xl border border-tally-layer-2  bg-tally-background/90 p-4"
+      )}
+    >
+      <div className="flex flex-shrink-0 flex-col">
+        <div className="flex items-center space-x-2">
+          {choice.icon ? (
+            <div className="relative h-[55px] w-[55px]">
+              <Image
+                src={choice.icon}
+                alt="something"
+                fill={true}
+                className="rounded-lg object-cover"
+              />
+            </div>
+          ) : null}
+          <div className="flex flex-col justify-between">
+            <div className="text-3xl font-bold text-white">{choice.title}</div>
+            <div className={cn(textCss, "text-lg")}>$.50</div>
+          </div>
+        </div>
+        <div className="mt-2 text-sm text-tally-gray">{`Total Pot: ${formatDollarsWithoutCents(
+          choice.total_pot
+        )}`}</div>
+      </div>
+      <div className="flex flex-col justify-between">
+        <div className="flex space-x-2">
+          <Input
+            className="w-[251px] border-0 bg-tally-layer-2 text-tally-gray placeholder:text-tally-gray"
+            placeholder="$0"
+          />
+          <Button className={cn(buttonCss, "text-black hover:text-black")}>
+            Buy
+          </Button>
+        </div>
+        <div className="flex items-end justify-between">
+          <div className="text-sm text-tally-gray">Shares</div>
+          <div className="text-white">0</div>
+        </div>
+        <div className="flex items-end justify-between">
+          <div className="text-sm text-tally-gray">Potential Return</div>
+          <div className="text-white">$0 (0%)</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function OrderCard({ choice }: { choice: ChoiceMarket }) {
+  let borderCss = "border-tally-primary hover:border-tally-primary/90";
+  if (choice.title === "No") {
+    borderCss = "border-tally-red hover:border-tally-red/90";
+  } else if (borderCssMap[choice.color as keyof typeof borderCssMap]) {
+    borderCss = borderCssMap[choice.color as keyof typeof borderCssMap];
+  }
+
+  let buttonCss = "bg-tally-primary hover:bg-tally-primary/90";
+  if (choice.title === "No") {
+    buttonCss = "bg-tally-red hover:bg-tally-red/90";
+  } else if (buttonCssMap[choice.color as keyof typeof buttonCssMap]) {
+    buttonCss = buttonCssMap[choice.color as keyof typeof buttonCssMap];
+  }
 
   let textCss = "text-tally-primary";
   if (choice.title === "No") {
@@ -128,9 +201,9 @@ export function OrderCardsMobile({ choices }: { choices: ChoiceMarket[] }) {
         />
       </div>
       {selected === "Yes" ? (
-        <OrderButton choice="Yes" />
+        <OrderCard choice="Yes" />
       ) : (
-        <OrderButton choice="No" />
+        <OrderCard choice="No" />
       )}
     </div>
   );
@@ -138,11 +211,22 @@ export function OrderCardsMobile({ choices }: { choices: ChoiceMarket[] }) {
 
 export function OrderCardsDesktop({ choices }: { choices: ChoiceMarket[] }) {
   if (!choices) return;
-  return (
-    <div className="hidden space-x-6 md:flex">
-      {choices.map((choice, index) => (
-        <OrderButton choice={choice} />
-      ))}
-    </div>
-  );
+  console.log(choices);
+  if (choices.length < 4) {
+    return (
+      <div className="hidden space-x-6 md:flex">
+        {choices.map((choice, index) => (
+          <OrderCard key={index} choice={choice} />
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div className="hidden md:grid md:grid-cols-2 md:gap-4">
+        {choices.map((choice, index) => (
+          <OrderCardGrid key={index} choice={choice} />
+        ))}
+      </div>
+    );
+  }
 }

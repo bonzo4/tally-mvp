122cb26228da596eacf32f4014424c55a3e671d2
Jimmy <jimmy.b.mau@gmail.com>
2024-01-23 13:47:20 -0500

    feat: display user profile page

--- src/app/profile/components/DisplayPicture.tsx
+++ src/app/profile/components/DisplayPicture.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 
-export default function DisplayPicture() {
+export default function DisplayPicture({
+  image,
+  alt,
+}: {
+  image: string;
+  alt?: string;
+}) {
   return (
     <div className="relative mr-4 h-[100px] w-[100px] lg:mr-12 lg:h-[200px] lg:w-[200px]">
       <Image
-        src="https://raw.githubusercontent.com/davidjerleke/embla-carousel/master/packages/embla-carousel-docs/src/assets/images/slide-2.jpg"
+        src={image}
         fill={true}
-        alt="test image"
+        alt={alt || "Display picture"}
         className="rounded-full object-cover"
       />
     </div>

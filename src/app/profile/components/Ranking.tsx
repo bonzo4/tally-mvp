8b174dd6bdd2cc2398eab2f4e9e6676f24d026db
Jimmy <jimmy.b.mau@gmail.com>
2024-01-23 13:47:05 -0500

    feat: rename ranking cards in profile page

--- src/app/profile/components/Ranking.tsx
+++ src/app/profile/components/Ranking.tsx
@@ -4,10 +4,9 @@ function PseudoMargin() {
   return <div className="min-w-[0px] lg:min-w-[40px]"></div>;
 }
 
-function Ranking() {
+function Ranking({ metric }: { metric?: string }) {
   const ranking = 2;
   const value = 1000;
-  const metric = "Profit";
   return (
     <div className="flex min-w-[40%] flex-col items-center space-y-1 rounded-2xl border-2 border-tally-primary p-4 md:w-full md:min-w-0">
       <div className="w-full text-center text-gray-400">{metric}</div>
@@ -23,11 +22,11 @@ export default function Rankings() {
   return (
     <div className="flex w-full items-center space-x-4 overflow-x-auto lg:space-x-6">
       <PseudoMargin />
-      <Ranking />
-      <Ranking />
-      <Ranking />
-      <Ranking />
-      <Ranking />
+      <Ranking metric="Portfolio Value" />
+      <Ranking metric="PNL" />
+      <Ranking metric="Volume" />
+      <Ranking metric="Markets Traded" />
+      <Ranking metric="Fair Launches" />
       <PseudoMargin />
     </div>
   );

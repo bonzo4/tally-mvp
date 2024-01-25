b16e9ef9faf40f86dace4024cabe6a7ebd2087c9
Jimmy <jimmy.b.mau@gmail.com>
2024-01-25 14:57:10 -0500

    style: make rank optional in stat cards

--- src/app/profile/components/Ranking.tsx
+++ src/app/profile/components/Ranking.tsx
@@ -14,23 +14,23 @@ function RankPrimitive({
   value,
 }: {
   metric: string;
-  rank: number;
+  rank?: number;
   value: string;
 }) {
   return (
-    <div className="flex min-w-[40%] flex-col items-center space-y-1 rounded-2xl border-2 border-tally-primary p-4 md:w-full md:min-w-0">
+    <div className="flex min-w-[40%] flex-col items-center justify-center space-y-1 rounded-2xl border-2 border-tally-primary p-4 md:w-full md:min-w-0">
       <div className="w-full text-center text-gray-400">{metric}</div>
       <div className="w-full text-center text-2xl font-medium text-white">
         {value}
       </div>
-      <div className="w-full text-center text-sm text-tally-primary">{`Rank ${formatNumberWithCommasNoDecimals(
-        rank
-      )}`}</div>
+      <div className="w-full text-center text-sm text-tally-primary">
+        {rank ? `Rank ${formatNumberWithCommasNoDecimals(rank)}` : ""}
+      </div>
     </div>
   );
 }
 
-function RankPortfolio({ value, rank }: { value: number; rank: number }) {
+function RankPortfolio({ value, rank }: { value: number; rank?: number }) {
   return (
     <RankPrimitive
       metric="Portfolio Value"
@@ -40,7 +40,7 @@ function RankPortfolio({ value, rank }: { value: number; rank: number }) {
   );
 }
 
-function RankPNL({ value, rank }: { value: number; rank: number }) {
+function RankPNL({ value, rank }: { value: number; rank?: number }) {
   return (
     <RankPrimitive
       metric="PNL"
@@ -50,7 +50,7 @@ function RankPNL({ value, rank }: { value: number; rank: number }) {
   );
 }
 
-function RankVolume({ value, rank }: { value: number; rank: number }) {
+function RankVolume({ value, rank }: { value: number; rank?: number }) {
   return (
     <RankPrimitive
       metric="Volume"
@@ -60,13 +60,13 @@ function RankVolume({ value, rank }: { value: number; rank: number }) {
   );
 }
 
-function RankMarkets({ value, rank }: { value: number; rank: number }) {
+function RankMarkets({ value, rank }: { value: number; rank?: number }) {
   return (
     <RankPrimitive metric="Markets" rank={rank} value={value.toString()} />
   );
 }
 
-function RankFairLaunches({ value, rank }: { value: number; rank: number }) {
+function RankFairLaunches({ value, rank }: { value: number; rank?: number }) {
   return (
     <RankPrimitive
       metric="Fair Launches"
@@ -90,16 +90,16 @@ export default function Rankings({
   volumeAndRank: RankByVolume;
 }) {
   return (
-    <div className="flex w-full items-center space-x-4 overflow-x-auto lg:space-x-6">
+    <div className="flex w-full items-stretch space-x-4 overflow-x-auto lg:space-x-6">
       <PseudoMargin />
-      <RankPortfolio value={portfolio} rank={32} />
-      <RankPNL value={pnl} rank={1324} />
+      <RankPortfolio value={portfolio} />
+      <RankPNL value={pnl} />
       <RankVolume
         value={volumeAndRank.total_volume}
         rank={volumeAndRank.rank}
       />
-      <RankMarkets value={markets} rank={1} />
-      <RankFairLaunches value={fairLaunches} rank={344} />
+      <RankMarkets value={markets} />
+      <RankFairLaunches value={fairLaunches} />
       <PseudoMargin />
     </div>
   );

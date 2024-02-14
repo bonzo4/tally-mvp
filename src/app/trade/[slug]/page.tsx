995ccca1399539b100be439a2616f3889982c782
Jimmy <jimmy.b.mau@gmail.com>
2024-02-14 13:08:04 -0500

    fix: mobile order drawer not working
    The issue was because we render both the mobile and desktop versions but
    just hide one of them. This means there are two of the same forms with
    the same input elements and ids. So when a user presses the radio
    buttons in the mobile version, it affects the hidden desktop version of
    the radio buttons.

--- src/app/trade/[slug]/page.tsx
+++ src/app/trade/[slug]/page.tsx
@@ -1,53 +1,12 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { createServerSupabaseClient } from "@/lib/supabase/server";
 
-import Chart from "./components/Chart";
-import Chat from "./components/Chat";
-import Order from "./components/Order";
-import OrderDrawer from "./components/OrderDrawer";
-import OrderBook from "./components/OrderBook";
-import Polls from "./components/Polls";
+import TradeBody from "./components/TradeBody";
 import RelatedMarkets from "./components/RelatedMarkets";
 import Slide from "@/components/Slide";
-import { createServerSupabaseClient } from "@/lib/supabase/server";
+
 import { getHoldings } from "@/lib/supabase/queries/holdings";
 import { getTradeMarkets } from "@/lib/supabase/queries/markets/tradeMarket";
 import { getUser } from "@/lib/supabase/queries/user";
-
-function TradingTabs() {
-  return (
-    <Tabs defaultValue="orderbook">
-      <TabsList className="flex justify-start space-x-4 bg-black">
-        <TabsTrigger
-          className="data-[state=active]: rounded-none border-tally-primary px-0 text-lg data-[state=active]:border-b data-[state=active]:bg-black data-[state=active]:text-tally-primary"
-          value="orderbook"
-        >
-          Order book
-        </TabsTrigger>
-        <TabsTrigger
-          className="data-[state=active]: rounded-none border-tally-primary px-0 text-lg data-[state=active]:border-b data-[state=active]:bg-black data-[state=active]:text-tally-primary"
-          value="chat"
-        >
-          Chat
-        </TabsTrigger>
-        <TabsTrigger
-          className="data-[state=active]: rounded-none border-tally-primary px-0 text-lg data-[state=active]:border-b data-[state=active]:bg-black data-[state=active]:text-tally-primary"
-          value="polls"
-        >
-          Polls
-        </TabsTrigger>
-      </TabsList>
-      <TabsContent value="orderbook">
-        <OrderBook />
-      </TabsContent>
-      <TabsContent value="chat">
-        <Chat />
-      </TabsContent>
-      <TabsContent value="polls">
-        <Polls />
-      </TabsContent>
-    </Tabs>
-  );
-}
 
 export default async function TradePage({
   params: { slug },
@@ -84,18 +43,7 @@ export default async function TradePage({
       <Slide slug={slug} />
       <div className="w-full px-4 pb-16 pt-4 lg:px-16">
         <div className="mb-10 flex w-full space-x-12">
-          <div className="flex flex-grow flex-col space-y-8 py-5">
-            <Chart />
-            <OrderDrawer
-              slug={slug}
-              user={user}
-              subMarkets={market.sub_markets}
-            />
-            <TradingTabs />
-          </div>
-          <div className="hidden py-5 lg:block">
-            <Order slug={slug} user={user} subMarkets={market.sub_markets} />
-          </div>
+          <TradeBody slug={slug} user={user} market={market} />
         </div>
         <RelatedMarkets />
       </div>
